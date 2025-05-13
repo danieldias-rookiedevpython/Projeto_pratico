0ebc9056542270b13f6a64bffb5a739a70be4c95
Daniel-Dias <daniel.dias007@cs.unipe.edu.br>
2025-05-13 08:23:28 -0300

    segundo commit

--- teste.py
+++ teste.py
@@ -3,3 +3,7 @@ atual = datetime.today().year
 usuario = int(input('Digite seu ano de nascimento: '))
 idade = atual - usuario
 print(f'Você tem {idade} anos.')
+if idade >= 10:
+    print('Você é maior de idade.')
+else:
+    print('Você é menor de idade.')
